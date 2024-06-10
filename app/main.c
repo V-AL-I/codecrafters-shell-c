8cddda4cde0d2fe87c845f579fc216afd5ec49c8
VALI <thomas.solatges@gmail.com>
2024-06-10 17:00:42 +0800

    done

--- app/main.c
+++ app/main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
+void displayErrorNotFound(char* command) {
+    printf("%s: command not found", command);
+}
+
 int main() {
-  // You can use print statements as follows for debugging, they'll be visible when running tests
-  //printf("Logs from your program will appear here!\n");
+    // You can use print statements as follows for debugging, they'll be visible when running tests
+    //printf("Logs from your program will appear here!\n");
 
-  //Uncomment this block to pass the first stage
-  printf("$ ");
-  fflush(stdout);
+    //Uncomment this block to pass the first stage
+    printf("$ ");
+    fflush(stdout);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  return 0;
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+    displayErrorNotFound(input);
+    
+    return 0;
 }
