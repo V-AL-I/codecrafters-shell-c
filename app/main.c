6f656366eaf8594c1e73b06f1709399e70b6c64b
VALI <thomas.solatges@gmail.com>
2024-06-10 16:26:13 +0800

    uncommented code

--- app/main.c
+++ app/main.c
@@ -4,9 +4,9 @@ int main() {
   // You can use print statements as follows for debugging, they'll be visible when running tests
   printf("Logs from your program will appear here!\n");
 
-  // Uncomment this block to pass the first stage
-  // printf("$ ");
-  // fflush(stdout);
+  //Uncomment this block to pass the first stage
+  printf("$ ");
+  fflush(stdout);
 
   // Wait for user input
   char input[100];
